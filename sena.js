8d41d993f9e3daf3943c62262b000b3c696dccc1
Ninna826 <dccardenas826@misena.edu.co>
2019-06-21 08:05:54 -0500

    aprendiendo for

--- sena.js
+++ sena.js
@@ -1,4 +1,58 @@
-var nombre= "Ninna "
-var apellido="Rossemund"
-alert("Hola "+nombre+" "+ apellido)
-console.log ("hola se subio el archivo correctamente")
+var arreglocarros= [
+{
+  marca: "BMW",
+  referencia: "M3",
+  modelo: 2015,
+  numchasis: 2345
+
+},
+{
+  marca: "AUDI",
+  referencia: "A3",
+  modelo: 2019,
+  numchasis: 4567	
+
+},
+{
+  marca: "CHEVROLET",
+  referencia: "CAMARO",
+  modelo: 2017,
+  numchasis: 56787
+},
+] 
+ for (var i = 0; i < arreglocarros.length; i++) 
+ {
+ 	if (arreglocarros[i].modelo < 2019)
+ 	alert(" su carro es el "+ arreglocarros[i].marca +" "+ arreglocarros[i].modelo+ " la tecnomecanica cuesta 100.000 " )
+ else 
+ 	alert (" su carro es el " + arreglocarros[i].marca+" "+ arreglocarros[i].modelo+ " su tecnomecanica cuesta 300.000 ")
+ 
+}
+
+	// alert(arreglocarros[i].marca+ " " +arreglocarros[i].modelo)
+
+
+
+
+
+
+// var persona ={
+//    nombre: "Ninna ",
+//    apellido: "Rossemund",
+//    cedula: "1015402298",
+//    edad: 22
+
+// }
+ 
+//  if(persona.edad >= 18)
+//  {
+//  	alert(persona.nombre+" "+" es mayor de edad y tiene "+persona.edad )
+//  }
+//  else
+//  {
+//  	alert(persona.nombre+" "+" es menor de edad ")
+//  }
+
+
+// alert("Hola "+nombre+" "+ apellido)
+// console.log ("hola se subio el archivo correctamente")
