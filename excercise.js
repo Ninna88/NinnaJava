f19e57a59f85005f2e03789631cdae90d43f9927
Ninna826 <dccardenas826@misena.edu.co>
2019-06-26 11:45:47 -0500

    metodos string

--- excercise.js
+++ excercise.js
@@ -1,67 +1,100 @@
-var arreglopersonas= [
-{
-  nombre: "Ninna ",
-  edad: 30,
-  cedula: 1022346628,
-  peliculafavorita: "NOW IS GOOD",
-  generopelicula: "Drama"
+var cadena1= "Carro bmw 2019"
+//var cadena2= "Nuevo"
 
-},
-{
-  nombre: "Marlon",
-  edad: 26,
-  cedula:1121903846 ,
-  peliculafavorita: "Rocky",
-  generopelicula: "Accion"	
+// console.log (cadena1.length)
 
-},
-{
-  nombre: "Rodolfo",
-  edad: 3,
-  cedula: 11232016,
-  peliculafavorita:"Aristogatos ",
-  generopelicula: "infantil" 
-}
-]
- console.log(arreglopersonas)
- for (var i = 0; i < arreglopersonas.length; i++) 
- {
-   alert ("Hola " +
-          arreglopersonas[i].nombre + " de " +
-          arreglopersonas[i].edad + " anos ")
-   if(arreglopersonas[i].edad >= 18)
-   {
-     switch (arreglopersonas[i].generopelicula)
-     { 
-       case 'Drama':
-       alert ("Las peliculas recomendadas son " + " Cenicienta por siempre " + " Quedate a mi lado " + " si decido quedarme ")
-       break;
-       case 'Terror':
-       alert("Peliculas recomendadas " + " The human centipede " + " Smiley " + " IT ")
-       break;
-       case 'Ficcion':
-       alert("Peliculas recomendadas "+" Avengers " + " IronMan 3 " + " Venon ")
-       break;
-       case 'Comedia':
-       alert("Peliculas recomendadas " + " Baywatch "+ " Tontos y mas tontos " + " La mascara ")
-       break;
-         case 'Accion':
-        alert ("Peliculas recomendadas " + " SherlockHolmes " + " GuerraMundial Z " + " Rec ")
-        break;
-       default:
-       alert("Sugerimos ingresar a la pagina para mas preferencias ");
-     }
-     } else if(arreglopersonas[i].edad < 18) 
-     {
-      switch (arreglopersonas[i].generopelicula)
-      {
-        case 'Accion infantil':
-        alert ("Peliculas recomendadas " + " HarryPotter " + " Valiente " + " GrandesHeroes ")
-        break;
-        case 'infantil':
-        alert("Peliculas recomendadas "+" Bichos " + " Ants " + " Mulan ")
-        break;
-      }
-     }
-   }
+// console.log (cadena1.charAt(0))
+// console.log (cadena1.charAt(1))
+// console.log (cadena1.charAt(2))
+// console.log (cadena1.charAt(3))
+// console.log (cadena1.charAt(4))
+//console.log (cadena1.toLowerCase())
+
+
+//console.log(cadena1.concat(cadena2))
+// var cadenaNueva=cadena1.concat(cadena2)
+// console.log(cadenaNueva)
+
+//console.log(cadena1.endsWith("o"))
+
+//console.log(cadena1.includes("rp"))
+//console.log(cadena1.indexOf("bmw"))
+
+//console.log(cadena1.slice(0,7))//imprime desde la primera letra hasta la ultima donde se le diga que corte
+
+//console.log(cadena1.split(" "))
+console.log(cadena1.substring(4,7))
+
+
+
+
+
+
+
+// var arreglopersonas= [
+// {
+//   nombre: "Ninna ",
+//   edad: 30,
+//   cedula: 1022346628,
+//   peliculafavorita: "NOW IS GOOD",
+//   generopelicula: "Drama"
+
+// },
+// {
+//   nombre: "Marlon",
+//   edad: 26,
+//   cedula:1121903846 ,
+//   peliculafavorita: "Rocky",
+//   generopelicula: "Accion"	
+
+// },
+// {
+//   nombre: "Rodolfo",
+//   edad: 3,
+//   cedula: 11232016,
+//   peliculafavorita:"Aristogatos ",
+//   generopelicula: "infantil" 
+// }
+// ]
+//  console.log(arreglopersonas)
+//  for (var i = 0; i < arreglopersonas.length; i++) 
+//  {
+//    alert ("Hola " +
+//           arreglopersonas[i].nombre + " de " +
+//           arreglopersonas[i].edad + " anos ")
+//    if(arreglopersonas[i].edad >= 18)
+//    {
+//      switch (arreglopersonas[i].generopelicula)
+//      { 
+//        case 'Drama':
+//        alert ("Las peliculas recomendadas son " + " Cenicienta por siempre " + " Quedate a mi lado " + " si decido quedarme ")
+//        break;
+//        case 'Terror':
+//        alert("Peliculas recomendadas " + " The human centipede " + " Smiley " + " IT ")
+//        break;
+//        case 'Ficcion':
+//        alert("Peliculas recomendadas "+" Avengers " + " IronMan 3 " + " Venon ")
+//        break;
+//        case 'Comedia':
+//        alert("Peliculas recomendadas " + " Baywatch "+ " Tontos y mas tontos " + " La mascara ")
+//        break;
+//          case 'Accion':
+//         alert ("Peliculas recomendadas " + " SherlockHolmes " + " GuerraMundial Z " + " Rec ")
+//         break;
+//        default:
+//        alert("Sugerimos ingresar a la pagina para mas preferencias ");
+//      }
+//      } else if(arreglopersonas[i].edad < 18) 
+//      {
+//       switch (arreglopersonas[i].generopelicula)
+//       {
+//         case 'Accion infantil':
+//         alert ("Peliculas recomendadas " + " HarryPotter " + " Valiente " + " GrandesHeroes ")
+//         break;
+//         case 'infantil':
+//         alert("Peliculas recomendadas "+" Bichos " + " Ants " + " Mulan ")
+//         break;
+//       }
+//      }
+//    }
 
